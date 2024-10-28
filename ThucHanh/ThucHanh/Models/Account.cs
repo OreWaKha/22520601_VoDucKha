944f2e0ee4e094d8bed15a01255040f0d06e8789
OreWaKha <161407020+OreWaKha@users.noreply.github.com>
2024-10-28 18:09:40 +0700

    Commit 1

---
+++ ThucHanh/ThucHanh/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThucHanh.Models;
+
+public partial class Account
+{
+    public int Id { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Pass { get; set; }
+}
